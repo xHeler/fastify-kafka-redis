1059a329c69cdefda7faa06dd4fb6a60e271cb26
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2024-07-25 15:11:21 +0200

    feat: add docker

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ server.get('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
